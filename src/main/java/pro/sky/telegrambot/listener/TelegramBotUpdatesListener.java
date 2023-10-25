e6ba716c7ab1cadb4b7d149fba7d4423a324aa88
03den <03denchik03@gmail.com>
2023-10-26 02:03:45 +0300

    проблемы с сохранением

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -9,14 +9,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.Notification;
+import pro.sky.telegrambot.repository.NotificationRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final NotificationRepository notificationRepository;
+
+    public TelegramBotUpdatesListener(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
 
     @Autowired
     private TelegramBot telegramBot;
@@ -30,15 +41,33 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-                if (update.message().text().equals("/start")) {
-                    long chatId = update.message().chat().id();
-                    SendMessage message = new SendMessage(chatId, "hi!");
-                    SendResponse response = telegramBot.execute(message);
-                }
+            if (update.message().text().equals("/start")) {
+                long chatId = update.message().chat().id();
+                SendMessage message = new SendMessage(chatId, "hi!");
+                SendResponse response = telegramBot.execute(message);
+            }
+            Pattern pattern = Pattern.compile("([0-9 .:\\s]{16})(\\s)([\\W+]+)");
+            Matcher matcher = pattern.matcher(update.message().text());
+            if (matcher.matches()) {
+                String date = matcher.group(1);
+                String item = matcher.group(3);
+                LocalDateTime time = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+                Notification notification = new Notification(update.message().chat().id(), item, time);
+//                add(matcher, update.message().chat().id());
+                SendMessage message = new SendMessage(update.message().chat().id(), notification.toString());
+                SendResponse response = telegramBot.execute(message);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    private Notification add(Matcher matcher, long chatId) {
+        String date = matcher.group(1);
+        String item = matcher.group(3);
+        LocalDateTime time = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+        return notificationRepository.save(new Notification(chatId, item, time));
+
+    }
 
 
 }
---
+++ src/main/java/pro/sky/telegrambot/model/Notification.java
@@ -0,0 +1,80 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private long chatId;
+    private String notification;
+    private LocalDateTime time;
+
+    public Notification(long chatId, String notification, LocalDateTime time) {
+        this.chatId = chatId;
+        this.notification = notification;
+        this.time = time;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notification that = (Notification) o;
+        return id == that.id && chatId == that.chatId && Objects.equals(notification, that.notification) && Objects.equals(time, that.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, chatId, notification, time);
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "id=" + id +
+                ", chatId=" + chatId +
+                ", notification='" + notification + '\'' +
+                ", time=" + time +
+                '}';
+    }
+}
